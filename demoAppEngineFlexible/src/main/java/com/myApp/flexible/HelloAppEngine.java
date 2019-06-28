23419f5c67058c3ef3a8516c5440390ee5ebc8ab
Vishakh Rameshan <vishakh.km.94@gmail.com>
2019-06-28 19:51:08 +0530

    commiting demo flexible app engine application

---
+++ demoAppEngineFlexible/src/main/java/com/myApp/flexible/HelloAppEngine.java
@@ -0,0 +1,26 @@
+package com.myApp.flexible;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("This is a Demo for App Engine Flexible - Hello App Engine Flexible!\r\n");
+
+  }
+}
